6f39f193238cf0d857b8c167b1e5bc96ebef5cc9
Mutsurini <simonedespirito99@gmail.com>
2026-02-17 12:40:49 +0100

    feat: Update script path and error handling
    Changes the script path in index.html from a relative path to a root-relative path. Also, improves error handling in index.tsx by logging a more informative message to the console if the root container is not found, instead of throwing an error.

--- index.tsx
+++ index.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const container = document.getElementById('root');
-if (!container) throw new Error('Root container not found');
-
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (!container) {
+  console.error('Root container "root" non trovato nel DOM.');
+} else {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
